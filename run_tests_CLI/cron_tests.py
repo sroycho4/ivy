79be0122fe806b431d2069e41da8bd1c9ba0aba8
Sujata R <135149476+sroycho4@users.noreply.github.com>
2023-09-29 10:21:25 +0530

    added new code

--- run_tests_CLI/cron_tests.py
+++ run_tests_CLI/cron_tests.py
@@ -3,15 +3,14 @@ from get_all_tests import get_all_tests
 
 run_iter = int(sys.argv[1])
 test_names = get_all_tests()
+
 # Run 150 tests in each iteration of the cron job
-num_tests = len(test_names)
 tests_per_run = 150
 start = run_iter * tests_per_run
 end = (run_iter + 1) * tests_per_run
+
 print("Running Tests:")
 with open("tests_to_run", "w") as f:
-    for i in range(start, end):
-        i = i % num_tests
-        test = test_names[i]
+    for test in test_names[start:end]:
         print(test)
         f.write(test + "\n")
